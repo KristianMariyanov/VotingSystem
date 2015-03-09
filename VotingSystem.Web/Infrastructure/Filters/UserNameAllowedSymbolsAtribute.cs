67383e220f2592fe0aa9577eff528c54782aea6e
Kristian Mariyanov <kristian.mariyanov@gmail.com>
2015-03-09 20:46:20 +0200

    add custom attribute for Username validation

---
+++ VotingSystem.Web/Infrastructure/Filters/UserNameAllowedSymbolsAtribute.cs
@@ -0,0 +1,31 @@
+﻿namespace VotingSystem.Web.Infrastructure.Filters
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public sealed class UserNameAllowedSymbolsAttribute : ValidationAttribute
+    {
+        private const string AllowedCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_";
+
+        public override bool IsValid(object value)
+        {
+            var username = value as string;
+
+            return username != null && this.ValidateSymbolsInUsername(username);
+        }
+
+        private bool ValidateSymbolsInUsername(string username)
+        {
+            for (int i = 0; i < username.Length; i++)
+            {
+                if (!AllowedCharacters.Contains(username[i].ToString()))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
