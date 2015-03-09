5bd4deeb78070af3aab5d8ed1d01071d8410a967
Kristian Mariyanov <kristian.mariyanov@gmail.com>
2015-03-09 20:36:38 +0200

    update Votes Repository and IVotesRepository.cs with method - GetVoteTitle By Id

--- VotingSystem.Data/Repositories/IVotesRepository.cs
+++ VotingSystem.Data/Repositories/IVotesRepository.cs
@@ -6,6 +6,8 @@
 
     public interface IVotesRepository : IGenericRepository<Vote>
     {
+        string TitleById(int id);
+
         IQueryable<Vote> AllActive();
 
         IQueryable<Vote> AllByUser(string userId);
--- VotingSystem.Data/Repositories/VotesRepository.cs
+++ VotingSystem.Data/Repositories/VotesRepository.cs
@@ -13,6 +13,11 @@
             
         }
 
+        public string TitleById(int id)
+        {
+            return this.context.Votes.Where(i => i.Id == id).Select(t => t.Title).FirstOrDefault();
+        }
+
         public IQueryable<Vote> AllActive()
         {
             return this.context.Votes.Where(v => v.EndDate > DateTime.Now && v.IsPublic);
