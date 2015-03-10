2ce8955728639f7ac3276710de0d104e90155d03
Kristian Mariyanov <kristian.mariyanov@gmail.com>
2015-03-10 18:45:27 +0200

    move CandidatesController to base Controller folder

---
+++ VotingSystem.Web/Controllers/CandidatesController.cs
@@ -0,0 +1,32 @@
+﻿namespace VotingSystem.Web.Controllers
+{
+    using System.Linq;
+    using System.Web.Mvc;
+
+    using AutoMapper.QueryableExtensions;
+
+    using VotingSystem.Data;
+    using VotingSystem.Web.Areas.User.ViewModels;
+
+    public class CandidatesController : BaseController
+    {
+        // GET: Candidates
+        public CandidatesController(IVotingSystemData data)
+            : base(data)
+        {
+        }
+
+        [HttpGet]
+        public ActionResult All(int id)
+        {
+            var candidates = this.Data
+                .Candidates
+                .AllByVote(id)
+                .Project()
+                .To<CandidateViewModel>()
+                .ToList();
+
+            return this.View(candidates);
+        }
+    }
+}
