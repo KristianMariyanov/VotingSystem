a962983d4477d058d9a3de1de0b3a3a615a0c173
Kristian Mariyanov <kristian.mariyanov@gmail.com>
2015-03-10 18:43:46 +0200

    add ViewModel for IdentificationCodes

---
+++ VotingSystem.Web/Areas/User/ViewModels/IdentificationCodeViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace VotingSystem.Web.Areas.User.ViewModels
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using VotingSystem.Models;
+    using VotingSystem.Web.Infrastructure.Mapping;
+
+    public class VoteIdentificationCodeViewModel : IMapFrom<IdentificationCode>
+    {
+        public int VoteId { get; set; }
+
+    }
+}
