8b2c370635ca9e2fab55bb7e2c1bd8b10e8bacda
Kristian Mariyanov <kristian.mariyanov@gmail.com>
2015-03-11 01:17:00 +0200

    add new route for vote with code

--- VotingSystem.Web/App_Start/RouteConfig.cs
+++ VotingSystem.Web/App_Start/RouteConfig.cs
@@ -2,6 +2,7 @@
 {
     using System.Web.Mvc;
     using System.Web.Routing;
+
     public class RouteConfig
     {
         public static void RegisterRoutes(RouteCollection routes)
@@ -9,11 +10,22 @@
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Vote with code",
+                url: "Votes/VoteWithCode/{code}",
+                defaults: new { controller = "Votes", action = "VoteWithCode" },
+                namespaces: new[] { "VotingSystem.Web.Controllers" });
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
-                namespaces: new[] { "VotingSystem.Web.Controllers" }
-            );
+                namespaces: new[] { "VotingSystem.Web.Controllers" });
+
+            routes.MapRoute(
+                    "Redirect",
+                    "",
+                    new { controller = "Home", action = "index" },
+                new[] { "VotingSystem.Web.Controllers" });
         }
     }
 }
