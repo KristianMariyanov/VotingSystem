bd68c3dac571a765e3563dbc583baace9d5d81d0
Kristian Mariyanov <kristian.mariyanov@gmail.com>
2015-03-11 01:20:12 +0200

    add working action for voteing with codes, add validation for valid code

--- VotingSystem.Web/Controllers/VotesController.cs
+++ VotingSystem.Web/Controllers/VotesController.cs
@@ -6,6 +6,7 @@
     using AutoMapper.QueryableExtensions;
 
     using VotingSystem.Data;
+    using VotingSystem.Models;
     using VotingSystem.Web.ViewModels.Candidates;
     using VotingSystem.Web.ViewModels.Votes;
 
@@ -31,24 +32,32 @@
         }
 
         [HttpGet]
-        public ActionResult Vote(int Id)
+        public ActionResult VoteWithCode(string code)
         {
-            var currentVote = this.Data.Votes.GetById(Id);
+            var codeInDb = this.Data.IdentificatonCodes.GetById(code);
+            if (codeInDb == null || codeInDb.VoteId == null || codeInDb.Used)
+            {
+                this.TempData["Error"] = "The Identification code is invalid ";
+                return this.RedirectToAction("Index", "Home");
+            }
+
+            var currentVote = this.Data.Votes.GetById(codeInDb.VoteId);
+            var voteWithCandidates = this.GetInputModelByVote(currentVote, code);
+
+            return this.View("Vote", voteWithCandidates);
+        }
+
+        [HttpGet]
+        public ActionResult Vote(int id)
+        {
+            var currentVote = this.Data.Votes.GetById(id);
             if (!currentVote.IsPublic)
             {
                 this.TempData["Error"] = "You cannot vote for private Vote without identification code";
                 return this.RedirectToAction("Index", "Home");
             }
-            var voteWithCandidates = new VoteWithCandidatesInputModel()
-            {
-                Id = currentVote.Id,
-                Title = currentVote.Title,
-                NumberOfVotes = currentVote.NumberOfVotes,
-                Candidates =
-                    this.Data.Candidates.AllByVote(Id)
-                    .Project()
-                    .To<CandidateInputModel>()
-            };
+
+            var voteWithCandidates = this.GetInputModelByVote(currentVote);
 
             return this.View(voteWithCandidates);
         }
@@ -59,13 +68,32 @@
         {
             if (model != null)
             {
-                var checkedCandidates = model.Candidates.Where(x => x.IsChecked);
                 var currentVote = this.Data.Votes.GetById(model.Id);
+                var useCode = false;
+                if (!currentVote.IsPublic)
+                {
+                    if (model.IdentificationCode != null)
+                    {
+                        var codeInDb = this.Data.IdentificatonCodes.GetById(model.IdentificationCode);
+                        if (codeInDb.VoteId == null || codeInDb.VoteId != currentVote.Id)
+                        {
+                            this.TempData["Error"] = "Invalid Identification Code";
+                        }
+
+                        useCode = true;
+                    }
+                    else
+                    {
+                        this.TempData["Error"] = "Invalid Identification Code";
+                    }
+                }
+
+                var checkedCandidates = model.Candidates.Where(x => x.IsChecked);
                 if (checkedCandidates.Count() != currentVote.NumberOfVotes)
                 {
-                    this.ModelState.AddModelError("Candidate", "does not match with restricted number of votes");
                     return this.View(model);
                 }
+
                 foreach (var candidate in model.Candidates)
                 {
                     if (candidate.IsChecked)
@@ -76,26 +104,49 @@
                             currentCandidate.VoteCount++;
 
                             this.Data.Candidates.Update(currentCandidate);
-                            this.Data.SaveChanges();
                         }
                         else
                         {
-                            this.ModelState.AddModelError("Candidate", "does not match with the Vote");
+                            this.TempData["Error"] = "Candidate does not match with the Vote";
                         }
-
                     }
                 }
 
-                this.RedirectToAction("All", "Votes", new { id = model.Id, Area = string.Empty });
+                if (useCode)
+                {
+                    var codeInDb = this.Data.IdentificatonCodes.GetById(model.IdentificationCode);
+                    codeInDb.Used = true;
+                    this.Data.IdentificatonCodes.Update(codeInDb);
+                }
+
+                this.Data.SaveChanges();
+                this.TempData["Success"] = "Your vote is accepted";
+                return this.RedirectToAction("All", "Votes");
             }
 
             return this.View(model);
         }
 
-        public ActionResult Details(int Id)
+        public ActionResult Details(int id)
         {
-
             return this.View();
+        }
+
+        private VoteWithCandidatesInputModel GetInputModelByVote(Vote vote, string code = null)
+        {
+            var viewModel = new VoteWithCandidatesInputModel()
+            {
+                Id = vote.Id,
+                Title = vote.Title,
+                NumberOfVotes = vote.NumberOfVotes,
+                Candidates =
+                    this.Data.Candidates.AllByVote(vote.Id)
+                    .Project()
+                    .To<CandidateInputModel>(),
+                IdentificationCode = code
+            };
+
+            return viewModel;
         }
     }
 }
